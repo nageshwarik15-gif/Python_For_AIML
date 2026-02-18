7d8051c2a3f5accb7d46fee574a08e8847d0c4c3
nageshwarik15-gif <nageshwarik15@gmail.com>
2026-02-18 21:43:25 +0530

    Update find_max.py

--- basic_problem_solving/find_max.py
+++ basic_problem_solving/find_max.py
@@ -51,3 +51,20 @@ print("Weeks =", weeks)
 -----------
 (5) 
 
+m1 = float(input("Enter marks of subject 1: "))
+m2 = float(input("Enter marks of subject 2: "))
+m3 = float(input("Enter marks of subject 3: "))
+m4 = float(input("Enter marks of subject 4: "))
+m5 = float(input("Enter marks of subject 5: "))
+
+total = m1 + m2 + m3 + m4 + m5
+
+average = total / 5
+
+# (Assuming each subject is out of 100)
+percentage = (total / 500) * 100
+
+print("Total Marks =", total)
+print("Average Marks =", average)
+print("Percentage =", percentage, "%")
+
