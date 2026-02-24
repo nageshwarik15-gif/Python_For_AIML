1a3dd49613ed2195d8108dc47fadc9d08afdaa59
nageshwarik15-gif <nageshwarik15@gmail.com>
2026-02-24 16:03:22 +0530

    Update 24_feb_26_matrix.py

--- 24_feb_26_matrix.py
+++ 24_feb_26_matrix.py
@@ -8,3 +8,13 @@ for i in range(len(A)):
         
 print (C)
 ----------
+A=[[1, 2],[3,4]]
+B=[[5, 6],[7,8]]
+C=A
+for i in range(len(A)):
+    for j in range (len(A)) :
+        C[i][j]=A[i][j]-B[i][j]
+ 
+        
+print (C)
+-----------
